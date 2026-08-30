3e897328a47b165b84dace42093fe0e033f9bdd9
Sanya Safi <sanya.safi@ufl.edu>
2026-08-30 12:44:08 -0500

    Add example products

---
+++ script.js
@@ -0,0 +1,18 @@
+// Example Products
+const products = [
+  { id: 1, name: "Printed Cotton Tote Bag", category: "Bags", price: "$22.00" },
+  {
+    id: 2,
+    name: "Crochet Coaster Set",
+    category: "Home Goods",
+    price: "$15.00",
+  },
+  {
+    id: 3,
+    name: "Chunky Crochet Flower Cardigan",
+    category: "Apparel",
+    price: "$65.00",
+  },
+  { id: 4, name: "Amethyst Bracelet", category: "Jewelry", price: "$20.00" },
+  { id: 5, name: "Sticker Sheet", category: "Bags", price: "$10.00" },
+];
